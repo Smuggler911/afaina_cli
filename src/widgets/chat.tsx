cbcda3dc21491ce55a7631edaa8401018e892c56
Smuggler911 <fellowgram@gmail.com>
2025-06-01 19:33:31 +0400

    feat(update):fetch

--- src/widgets/chat.tsx
+++ src/widgets/chat.tsx
@@ -59,7 +59,7 @@ export const Chat: React.FC =()=>{
 
         setText('');
 
-        const response = await fetch("http://127.0.0.1:3000/send", {
+        const response = await fetch("/send", {
             method: "POST",
             headers: {
                 "Content-Type": "application/json"
